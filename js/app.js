19bf5219e1f772f9644bf6e6c55779f6366c6f1b
Silviahemmet <jakob.hansen@folkhalsan.fi>
2026-07-24 08:39:26 +0300

    Add alert to indicate JS functionality

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // Steg 12.3 - Open-Meteo + Clock
 // MASTER design remains untouched
 // ======================================
-
+alert("JS fungerar!");
 function updateClock() {
     const now = new Date();
 
